55aeafd3187a5ef513fc11685bd835767ad35a57
nicho <nicho@wcs.co.id>
2025-05-20 21:35:23 +0700

    db dse

--- db_dse.sql
+++ db_dse.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: May 18, 2025 at 08:14 PM
+-- Generation Time: May 20, 2025 at 04:34 PM
 -- Server version: 10.4.11-MariaDB
 -- PHP Version: 7.3.15
 
@@ -42,8 +42,8 @@ CREATE TABLE `number_sequences` (
 --
 
 INSERT INTO `number_sequences` (`recid`, `prefix`, `kode_perusahaan`, `bulan`, `tahun`, `last_number`) VALUES
-(1, 'PO', 'DSE', 1, 2025, '1'),
-(7, 'INV', 'DSE', 1, 2025, '0');
+(7, 'INV', 'DSE', 1, 2025, '0'),
+(8, 'PO', 'DSE', 5, 2025, '2');
 
 -- --------------------------------------------------------
 
@@ -68,7 +68,8 @@ INSERT INTO `tbl_bahan_baku` (`recid`, `nama_bb`, `desc`, `stok`, `satuan`, `sup
 (2, 'bahan baku', 'tes', 6000, 'Kg', 1),
 (3, 'bahan baku  TOKO B', 'bahan baku TOKO B', 6100, 'Kg', 2),
 (8, 'tes', 'tes', 5000, 'Liter', 1),
-(9, 'tess', 'sasaas', 500, 'Kg', 1);
+(9, 'tess', 'sasaas', 500, 'Kg', 1),
+(22, 'tes bimbingan', 'tes bimbingan', 505, 'Kg', 1);
 
 -- --------------------------------------------------------
 
@@ -91,7 +92,7 @@ CREATE TABLE `tbl_client` (
 --
 
 INSERT INTO `tbl_client` (`recid`, `nama_client`, `alamat`, `status`, `email`, `no_telp`, `fax`) VALUES
-(1, 'PT Erricon', 'Cibarusah', 1, 'erricon@gmail.com', 123, 312);
+(1, 'PT Erricon', 'Cibarusah', 0, 'erricon@gmail.com', 123, 312);
 
 -- --------------------------------------------------------
 
@@ -114,8 +115,8 @@ CREATE TABLE `tbl_formulasi` (
 INSERT INTO `tbl_formulasi` (`recid`, `produk_id`, `bahanbaku_id`, `qty_per_ton`, `uom`) VALUES
 (11, 1, 3, '500.00', 'Kg'),
 (13, 1, 2, '500.00', 'Kg'),
-(14, 2, 8, '50.00', 'Liter'),
-(15, 2, 9, '50.00', 'Kg');
+(16, 5, 2, '500.00', 'Kg'),
+(17, 5, 8, '500.00', 'Liter');
 
 -- --------------------------------------------------------
 
@@ -138,8 +139,7 @@ CREATE TABLE `tbl_inventaris` (
 
 INSERT INTO `tbl_inventaris` (`recid`, `nama_inven`, `desc`, `jml`, `jml_rusak`, `jml_active`) VALUES
 (1, 'Kempu', 'Alat penampungan obat', 10, 3, 7),
-(2, 'Pompa', 'Alat pemindah obat', 10, 7, 3),
-(5, 'iitng', 'iting', 136, 35, 101);
+(2, 'Pompa', 'Alat pemindah obat', 10, 7, 3);
 
 -- --------------------------------------------------------
 
@@ -162,8 +162,7 @@ CREATE TABLE `tbl_product` (
 
 INSERT INTO `tbl_product` (`recid`, `nama_product`, `desc_product`, `grade`, `level`, `hargaPerTon`) VALUES
 (1, 'rekton type F', 'untuk percepatan pengerasan beton K-125 di bawah 6 jam', 'F', '1', 1000000),
-(2, 'tessss', 'tes ajaaaa', 'B', '4', 2000000),
-(4, 'tesz', 'tesaz', 'A', '2', 3000000);
+(5, 'product baru DSE', 'product baru 2025', 'A', '1', 1000000);
 
 -- --------------------------------------------------------
 
@@ -252,12 +251,12 @@ CREATE TABLE `tbl_transaksi_bahanbaku` (
   `supp_id` int(11) DEFAULT NULL,
   `jumlah_bayar` double DEFAULT NULL,
   `bukti_file` varchar(255) DEFAULT NULL,
-  `tgl_byr` date NOT NULL,
+  `tgl_byr` date DEFAULT NULL,
   `status_bayar` tinyint(1) DEFAULT NULL,
   `qty_terima` int(11) DEFAULT NULL,
   `tgl_jatuhtempo` date DEFAULT NULL,
   `sudah_diterima` tinyint(4) NOT NULL DEFAULT 0,
-  `catatan_terima` varchar(100) NOT NULL,
+  `catatan_terima` varchar(100) DEFAULT NULL,
   `createdby` varchar(50) DEFAULT NULL,
   `modifiedby` varchar(50) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
@@ -267,14 +266,10 @@ CREATE TABLE `tbl_transaksi_bahanbaku` (
 --
 
 INSERT INTO `tbl_transaksi_bahanbaku` (`recid`, `tgl`, `no_po`, `no_invoice`, `buktibayar`, `harga`, `pengiriman`, `status`, `qty`, `uom`, `bahanbaku_id`, `supp_id`, `jumlah_bayar`, `bukti_file`, `tgl_byr`, `status_bayar`, `qty_terima`, `tgl_jatuhtempo`, `sudah_diterima`, `catatan_terima`, `createdby`, `modifiedby`) VALUES
-(1, '2025-05-07', '202020', '20202', 'tes', '20000.00', 'tes', 0, 100, '', 2, 1, NULL, NULL, '0000-00-00', 1, NULL, '2025-05-07', 0, '', NULL, NULL),
-(11, '2025-05-10', NULL, NULL, NULL, NULL, NULL, 0, 500, 'Kg', 9, NULL, NULL, NULL, '0000-00-00', NULL, NULL, NULL, 0, '', NULL, NULL),
-(12, '2025-05-10', NULL, NULL, NULL, NULL, NULL, 0, 500, 'Kg', 9, NULL, NULL, NULL, '0000-00-00', NULL, NULL, NULL, 0, '', NULL, NULL),
-(13, '2025-05-16', '0001/PO/DSE/1/2025', NULL, NULL, NULL, NULL, 1, 44, 'Kg', 3, NULL, NULL, NULL, '0000-00-00', NULL, NULL, NULL, 0, '', 'admin', 'admin'),
-(14, '2025-05-16', NULL, NULL, NULL, NULL, NULL, 0, 44, 'Kg', 3, NULL, NULL, NULL, '0000-00-00', NULL, NULL, NULL, 0, '', 'admin', 'admin'),
+(13, '2025-05-16', NULL, NULL, NULL, NULL, NULL, 0, 44, 'Kg', 3, NULL, NULL, NULL, '0000-00-00', NULL, NULL, NULL, 0, '', 'admin', 'admin'),
 (15, '2025-05-16', '0005/PO/DSE/1/2025', '0001/INV/tokoooo/1/2025', NULL, '5000.00', NULL, 2, 5556, 'Liter', 3, NULL, 100000, 'Screenshot 2025-04-21 at 04.48.17.png', '2025-05-16', 1, 6000, NULL, 0, 'tes', 'admin', 'admin'),
-(16, '2025-05-16', NULL, NULL, NULL, NULL, NULL, 0, 44444, 'Kg', 3, 1, NULL, NULL, '0000-00-00', NULL, NULL, NULL, 0, '', 'admin', 'admin'),
-(17, '2025-05-16', '0004/PO/DSE/1/2025', '0004/PO/TOKO-B/1/2025', NULL, '1000000.00', NULL, 1, 906, 'Kg', 3, 2, NULL, NULL, '0000-00-00', NULL, NULL, NULL, 0, '', 'admin', 'admin');
+(17, '2025-05-16', '0004/PO/DSE/1/2025', '0004/PO/TOKO-B/1/2025', NULL, '1000000.00', NULL, 1, 906, 'Kg', 3, 2, NULL, NULL, '0000-00-00', NULL, NULL, NULL, 0, '', 'admin', 'admin'),
+(19, '2025-05-20', '0002/PO/DSE-05/2025', '0004/PO/TOKO-B/1/2025', NULL, '20000.00', NULL, 2, 500, 'Kg', 22, 1, 100000, 'Screenshot 2025-04-21 at 04.48.17.png', '2025-05-21', 1, 505, NULL, 0, 'gud', 'admin', 'admin');
 
 -- --------------------------------------------------------
 
@@ -314,7 +309,7 @@ CREATE TABLE `transaksi_keluar` (
   `buktibayar` varchar(100) DEFAULT NULL,
   `harga` decimal(15,2) DEFAULT NULL,
   `ppn` decimal(15,2) DEFAULT NULL,
-  `qty` int(11) DEFAULT NULL,
+  `qty` decimal(10,2) DEFAULT NULL,
   `total_harga` decimal(15,2) DEFAULT NULL,
   `pengiriman` varchar(100) DEFAULT NULL,
   `ongkir` decimal(15,2) DEFAULT NULL,
@@ -322,7 +317,7 @@ CREATE TABLE `transaksi_keluar` (
   `tanggal_sampai` date DEFAULT NULL,
   `tgl_jatuh_tempo` date DEFAULT NULL,
   `status_pembayaran` tinyint(1) DEFAULT NULL,
-  `sisa_pembayaran` decimal(15,2) DEFAULT NULL,
+  `sisa_pembayaran` int(11) DEFAULT NULL,
   `sudah_diterima` tinyint(4) NOT NULL,
   `pakai_inventaris` tinyint(1) DEFAULT NULL,
   `inven_id` int(11) DEFAULT NULL,
@@ -355,7 +350,8 @@ CREATE TABLE `uom` (
 
 INSERT INTO `uom` (`recid`, `kode_uom`, `nama_uom`, `batas_aman`) VALUES
 (1, 'Kg', 'Kilo Gram', 500),
-(2, 'Liter', 'Liter', 400);
+(2, 'Liter', 'Liter', 400),
+(3, 'Ton', 'Tonase', 1);
 
 --
 -- Indexes for dumped tables
@@ -469,13 +465,13 @@ ALTER TABLE `uom`
 -- AUTO_INCREMENT for table `number_sequences`
 --
 ALTER TABLE `number_sequences`
-  MODIFY `recid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `recid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT for table `tbl_bahan_baku`
 --
 ALTER TABLE `tbl_bahan_baku`
-  MODIFY `recid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `recid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
 
 --
 -- AUTO_INCREMENT for table `tbl_client`
@@ -487,7 +483,7 @@ ALTER TABLE `tbl_client`
 -- AUTO_INCREMENT for table `tbl_formulasi`
 --
 ALTER TABLE `tbl_formulasi`
-  MODIFY `recid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+  MODIFY `recid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
 
 --
 -- AUTO_INCREMENT for table `tbl_inventaris`
@@ -499,31 +495,31 @@ ALTER TABLE `tbl_inventaris`
 -- AUTO_INCREMENT for table `tbl_product`
 --
 ALTER TABLE `tbl_product`
-  MODIFY `recid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `recid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `tbl_relasi_inven`
 --
 ALTER TABLE `tbl_relasi_inven`
-  MODIFY `recid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `recid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `tbl_supplier`
 --
 ALTER TABLE `tbl_supplier`
-  MODIFY `recid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `recid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `tbl_tmpt_produksi`
 --
 ALTER TABLE `tbl_tmpt_produksi`
-  MODIFY `recid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `recid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `tbl_transaksi_bahanbaku`
 --
 ALTER TABLE `tbl_transaksi_bahanbaku`
-  MODIFY `recid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+  MODIFY `recid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
 
 --
 -- AUTO_INCREMENT for table `tbl_user`
@@ -535,13 +531,13 @@ ALTER TABLE `tbl_user`
 -- AUTO_INCREMENT for table `transaksi_keluar`
 --
 ALTER TABLE `transaksi_keluar`
-  MODIFY `recid` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `recid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `uom`
 --
 ALTER TABLE `uom`
-  MODIFY `recid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `recid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- Constraints for dumped tables
